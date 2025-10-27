3c76beadf73c550c0102acb1da35e6738b3da664
izawartka <59137928+izawartka@users.noreply.github.com>
2025-10-27 23:18:52 +0100

    Refactor MenuManager
    - Refactored ModMenuModule::MenuManager to use pending changes queue. All menu operations can now be nested and their execution should be safe

--- src/modules/modmenu/menu-manager.cpp
+++ src/modules/modmenu/menu-manager.cpp
@@ -14,37 +14,40 @@ ModMenuModule::MenuManager::MenuManager() {
 }
 
 ModMenuModule::MenuManager::~MenuManager() {
-	ClearMenus();
 	m_instance = nullptr;
+}
+
+void ModMenuModule::MenuManager::RemoveMenu(MenuId id)
+{
+	auto it = std::find(m_menuIds.begin(), m_menuIds.end(), id);
+	if (it == m_menuIds.end()) {
+		spdlog::warn("Attempted to remove non-existent menu with id {}", id);
+		return;
+	}
+	m_menuIds.erase(it);
+	PendingChange change = { ChangeType::Remove, id, nullptr };
+	AddPendingChange(change);
 }
 
 void ModMenuModule::MenuManager::RemoveLastMenu()
 {
-	if (m_menus.empty()) {
+	if (m_menuIds.empty()) {
 		spdlog::warn("Attempted to remove menu from empty stack");
 		return;
 	}
-	MenuBase* backMenu = m_menus.back().get();
-	backMenu->SetVisible(false);
-	backMenu->Detach();
-	m_menus.pop_back();
-
-	if (m_menus.empty()) {
-		return;
-	}
-
-	ModMenuModule::MenuBase* menu = m_menus.back().get();
-	menu->Attach();
-	menu->SetVisible(m_visible);
+	MenuId lastMenuId = m_menuIds.back();
+	m_menuIds.pop_back();
+	PendingChange change = { ChangeType::Remove, lastMenuId, nullptr };
+	AddPendingChange(change);
 }
 
 void ModMenuModule::MenuManager::ClearMenus()
 {
-	while (!m_menus.empty()) {
-		MenuBase* backMenu = m_menus.back().get();
-		backMenu->SetVisible(false);
-		backMenu->Detach();
-		m_menus.pop_back();
+	std::vector<MenuId> menuIds = m_menuIds;
+	m_menuIds.clear();
+	for (MenuId id : menuIds) {
+		PendingChange change = { ChangeType::Remove, id, nullptr };
+		AddPendingChange(change);
 	}
 }
 
@@ -53,13 +56,15 @@ void ModMenuModule::MenuManager::SetVisible(bool visible)
 	if (visible == m_visible) {
 		return;
 	}
+
 	m_visible = visible;
 
 	if (m_menus.empty()) {
 		return;
 	}
 
-	m_menus.back()->SetVisible(visible);
+	PendingChange change = { ChangeType::UpdateVisible, 0, nullptr };
+	AddPendingChange(change);
 }
 
 void ModMenuModule::MenuManager::OnKeyDown(KeyDownEvent& event)
@@ -76,5 +81,119 @@ void ModMenuModule::MenuManager::Attach() {
 
 void ModMenuModule::MenuManager::Detach() {
 	RemoveEventListener<KeyDownEvent>();
+	if (m_pendingChanges.size()) {
+		spdlog::warn("MenuManager is being detached with {} pending changes", m_pendingChanges.size());
+	}
+	SetVisible(false);
 	ClearMenus();
+}
+
+void ModMenuModule::MenuManager::ApplyMenuAdd(PendingChange& change)
+{
+	MenuBase* newMenu = static_cast<MenuBase*>(change.menu);
+
+	auto it = std::find_if(m_menus.begin(), m_menus.end(),
+		[change](const MenuStackEntry& entry) { return entry.id == change.id; });
+
+	if (it != m_menus.end()) {
+		spdlog::warn("MenuManager::ApplyMenuAdd: Menu with id {} already exists", change.id);
+		return;
+	}
+
+	DetachTopMenu();
+	spdlog::debug("MenuManager::ApplyMenuAdd: Adding menu with id {}", change.id);
+	m_menus.push_back({ change.id, std::unique_ptr<MenuBase>(newMenu) });
+}
+
+void ModMenuModule::MenuManager::ApplyMenuRemove(PendingChange& change)
+{
+	auto it = std::find_if(m_menus.begin(), m_menus.end(),
+		[change](const MenuStackEntry& entry) { return entry.id == change.id; });
+	if (it == m_menus.end()) {
+		spdlog::warn("MenuManager::ApplyMenuRemove: Menu with id {} does not exist", change.id);
+		return;
+	}
+
+	if (it == m_menus.end() - 1) {
+		DetachTopMenu();
+	}
+
+	spdlog::debug("MenuManager::ApplyMenuRemove: Removing menu with id {}", change.id);
+	m_menus.erase(it);
+}
+
+void ModMenuModule::MenuManager::DetachTopMenu()
+{
+	if (m_menus.empty()) {
+		return;
+	}
+
+	MenuStackEntry& topMenu = m_menus.back();
+	if (m_topMenuAttached) {
+		spdlog::debug("MenuManager::DetachTopMenu: Detaching top menu with id {}", topMenu.id);
+		topMenu.menu->Detach();
+		m_topMenuAttached = false;
+	}
+}
+
+void ModMenuModule::MenuManager::ProcessPendingChanges()
+{
+	if (m_pendingChanges.empty()) {
+		return;
+	}
+
+	m_processingChanges = true;
+
+	MenuStackEntry* previousTopMenu = m_menus.empty() ? nullptr : &m_menus.back();
+	if (previousTopMenu && previousTopMenu->menu->IsVisible()) {
+		spdlog::debug("MenuManager::ProcessPendingChanges: Hiding top menu with id {}", previousTopMenu->id);
+		previousTopMenu->menu->SetVisible(false);
+		m_topMenuVisible = false;
+	}
+
+	while (!m_pendingChanges.empty()) {
+		PendingChange change = m_pendingChanges.front();
+		m_pendingChanges.pop();
+		switch (change.type) {
+		case ChangeType::Add:
+			ApplyMenuAdd(change);
+			break;
+		case ChangeType::Remove:
+			ApplyMenuRemove(change);
+			break;
+		case ChangeType::UpdateVisible:
+			break;
+		default:
+			spdlog::warn("MenuManager::ProcessPendingChanges: Unknown change type");
+			break;
+		}
+	}
+
+	MenuStackEntry* newTopMenu = m_menus.empty() ? nullptr : &m_menus.back();
+
+	if(!m_topMenuAttached && newTopMenu) {
+		spdlog::debug("MenuManager::ProcessPendingChanges: Attaching top menu with id {}", newTopMenu->id);
+		newTopMenu->menu->Attach();
+		m_topMenuAttached = true;
+	}
+
+	if(!m_topMenuVisible && newTopMenu && m_visible) {
+		spdlog::debug("MenuManager::ProcessPendingChanges: Setting top menu with id {} visible", newTopMenu->id);
+		newTopMenu->menu->SetVisible(true);
+		m_topMenuVisible = true;
+	}
+
+	m_processingChanges = false;
+
+	if(!m_pendingChanges.empty()) {
+		ProcessPendingChanges();
+	}
+}
+
+void ModMenuModule::MenuManager::AddPendingChange(const PendingChange& change)
+{
+	m_pendingChanges.push(change);
+	if (!m_processingChanges) {
+		ProcessPendingChanges();
+	}
 }
--- src/modules/modmenu/menu-manager.h
+++ src/modules/modmenu/menu-manager.h
@@ -4,33 +4,24 @@
 #include "../../events/keyboard.h"
 
 namespace ModMenuModule {
+	using MenuId = size_t;
+
 	class MenuManager : public Core::EventListenerSupport {
 	public:
 		static MenuManager* GetInstance();
 
 		template<typename MenuT, typename... Args>
-		MenuT* AddMenu(Args&&... args) {
+		MenuId AddMenu(Args&&... args) {
 			static_assert(std::is_base_of_v<MenuBase, MenuT>, "MenuT must be derived from MenuBase");
-
 			MenuT* newMenu = new MenuT(std::forward<Args>(args)...);
-			if (!newMenu->Attach()) {
-				spdlog::error("Failed to attach mod menu {}", typeid(MenuT).name());
-				delete newMenu;
-				return nullptr;
-			}
-			if (m_visible) {
-				newMenu->SetVisible(true);
-			}
-
-			if (!m_menus.empty()) {
-				m_menus.back()->SetVisible(false);
-				m_menus.back()->Detach();
-			}
-
-			m_menus.emplace_back(std::unique_ptr<MenuBase>(newMenu));
-			return newMenu;
+			MenuId id = m_nextMenuId++;
+			m_menuIds.push_back(id);
+			PendingChange change = { ChangeType::Add, id, newMenu };
+			AddPendingChange(change);
+			return id;
 		}
 
+		void RemoveMenu(MenuId id);
 		void RemoveLastMenu();
 		void ClearMenus();
 		void SetVisible(bool visible);
@@ -48,7 +39,32 @@ namespace ModMenuModule {
 		void Attach();
 		void Detach();
 
-		std::vector<std::unique_ptr<MenuBase>> m_menus;
+		enum class ChangeType { Add, Remove, UpdateVisible };
+		struct PendingChange {
+			ChangeType type;
+			MenuId id = 0;
+			MenuBase* menu = nullptr; // Only used for Add
+		};
+
+		std::queue<PendingChange> m_pendingChanges;
+
+		void ApplyMenuAdd(PendingChange& change);
+		void ApplyMenuRemove(PendingChange& change);
+		void DetachTopMenu();
+		void ProcessPendingChanges();
+		void AddPendingChange(const PendingChange& change);
+
+		struct MenuStackEntry {
+			MenuId id;
+			std::unique_ptr<MenuBase> menu;
+		};
+
+		std::vector<MenuId> m_menuIds;
+		std::vector<MenuStackEntry> m_menus;
 		bool m_visible = false;
+		bool m_processingChanges = false;
+		bool m_topMenuAttached = false;
+		bool m_topMenuVisible = false;
+		MenuId m_nextMenuId = 1;
 	};
 }
