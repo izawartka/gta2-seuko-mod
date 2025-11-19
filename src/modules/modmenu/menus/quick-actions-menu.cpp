f8be127bb1118f63cf6c5d3a4fe1ca0ebe0b4133
izawartka <59137928+izawartka@users.noreply.github.com>
2025-11-19 12:12:53 +0100

    Add QuickActionsMenu paging
    - Added paging support for ModMenuModule::QuickActionsMenu

--- src/modules/modmenu/menus/quick-actions-menu.cpp
+++ src/modules/modmenu/menus/quick-actions-menu.cpp
@@ -4,9 +4,9 @@
 #include "quick-action-options-menu.h"
 #include "../../../converters/key.h"
 
-ModMenuModule::QuickActionsMenu::QuickActionsMenu()
+ModMenuModule::QuickActionsMenu::QuickActionsMenu(size_t page)
 {
-
+	m_page = page;
 }
 
 ModMenuModule::QuickActionsMenu::~QuickActionsMenu()
@@ -16,18 +16,38 @@ ModMenuModule::QuickActionsMenu::~QuickActionsMenu()
 
 bool ModMenuModule::QuickActionsMenu::Attach()
 {
+	ModMenuModule::QuickActionManager* quickActionManager = ModMenuModule::QuickActionManager::GetInstance();
+	auto actionIds = quickActionManager->GetAll();
+	size_t pageCount = (actionIds.size() + ACTIONS_PER_PAGE - 1) / ACTIONS_PER_PAGE;
+
+	if(m_page >= pageCount && pageCount > 0) {
+		m_page = pageCount - 1;
+	}
+
 	UiModule::Component* vertCont;
-	CreateMenu(L"#Quick actions#", vertCont);
+	CreateMenu(GetPageTitle(pageCount), vertCont);
 	UiModule::RootModule* uiRoot = UiModule::RootModule::GetInstance();
 	ModMenuModule::ModMenuOptions options = ModMenuModule::RootModule::GetInstance()->GetOptions();
 
 	m_menuController->CreateItem<UiModule::Text>(vertCont, L"Go back", options.textSize);
 	m_menuController->CreateItem<UiModule::Text>(vertCont, L"Add quick action", options.textSize);
 
-	ModMenuModule::QuickActionManager* quickActionManager = ModMenuModule::QuickActionManager::GetInstance();
-	auto actionIds = quickActionManager->GetAll();
+	m_prevPageItemId = -1;
+	m_nextPageItemId = -1;
+	if (m_page > 0) {
+		m_menuController->CreateItem<UiModule::Text>(vertCont, L"Previous page", options.textSize);
+		m_prevPageItemId = m_menuController->GetLatestMenuItemId();
+	}
+	if (m_page < pageCount - 1) {
+		m_menuController->CreateItem<UiModule::Text>(vertCont, L"Next page", options.textSize);
+		m_nextPageItemId = m_menuController->GetLatestMenuItemId();
+	}
 
-	for (ModMenuModule::QuickActionId actionId : actionIds) {
+	uiRoot->AddComponent<UiModule::Spacer>(vertCont, 0, options.menuSpacerHeight);
+
+	m_firstActionItemId = -1;
+	for (size_t i = m_page * ACTIONS_PER_PAGE; i < std::min(actionIds.size(), (m_page + 1) * ACTIONS_PER_PAGE); i++) {
+		ModMenuModule::QuickActionId actionId = actionIds[i];
 		std::optional<ModMenuModule::QuickActionInfo> actionInfoOpt = quickActionManager->GetInfo(actionId);
 		if (!actionInfoOpt.has_value()) {
 			continue;
@@ -35,7 +55,6 @@ bool ModMenuModule::QuickActionsMenu::Attach()
 		ModMenuModule::QuickActionInfo actionInfo = actionInfoOpt.value();
 		std::wstring itemLabel = actionInfo.GetLabel() + L" (" + KeyConverter<true>::ConvertToString(actionInfo.key) + L")";
 		m_menuController->CreateItem<UiModule::Text>(vertCont, itemLabel, options.textSize);
-
 		if(m_firstActionItemId == -1) {
 			m_firstActionItemId = m_menuController->GetLatestMenuItemId();
 		}
@@ -48,21 +67,45 @@ bool ModMenuModule::QuickActionsMenu::Attach()
 
 void ModMenuModule::QuickActionsMenu::OnMenuAction(UiModule::Selectable* item, UiModule::MenuItemId id)
 {
+	ModMenuModule::MenuManager* menuManager = ModMenuModule::MenuManager::GetInstance();
+
 	switch (id) {
 	case 0: // Go back
-		ModMenuModule::MenuManager::GetInstance()->RemoveLastMenu();
+		menuManager->RemoveLastMenu();
 		return;
 	case 1: // Add quick action
-		ModMenuModule::MenuManager::GetInstance()->AddMenu<AddQuickActionMenu>();
+		menuManager->AddMenu<AddQuickActionMenu>();
 		return;
 	default:
 		break;
 	}
 
-	if (id >= m_firstActionItemId && m_firstActionItemId != -1) {
+	if (id == m_prevPageItemId) { // Previous page
+		MenuId thisMenuId = menuManager->GetTopMenuId();
+		menuManager->AddMenu<QuickActionsMenu>(m_page - 1);
+		menuManager->RemoveMenu(thisMenuId);
+		return;
+	}
+	else if (id == m_nextPageItemId) { // Next page
+		MenuId thisMenuId = menuManager->GetTopMenuId();
+		menuManager->AddMenu<QuickActionsMenu>(m_page + 1);
+		menuManager->RemoveMenu(thisMenuId);
+		return;
+	}
+	else if (id >= m_firstActionItemId && m_firstActionItemId != -1) {
 		ModMenuModule::QuickActionManager* quickActionManager = ModMenuModule::QuickActionManager::GetInstance();
 		auto actionIds = quickActionManager->GetAll();
-		ModMenuModule::QuickActionId actionId = actionIds[id - m_firstActionItemId];
-		ModMenuModule::MenuManager::GetInstance()->AddMenu<QuickActionOptionsMenu>(actionId);
+		ModMenuModule::QuickActionId actionId = actionIds[id - m_firstActionItemId + m_page * ACTIONS_PER_PAGE];
+		menuManager->AddMenu<QuickActionOptionsMenu>(actionId);
+	}
+}
+
+std::wstring ModMenuModule::QuickActionsMenu::GetPageTitle(size_t pageCount) const
+{
+	if (pageCount > 1) {
+		return L"#Quick actions (" + std::to_wstring(m_page + 1) + L"/" + std::to_wstring(pageCount) + L")#";
+	}
+	else {
+		return L"#Quick actions";
 	}
 }
--- src/modules/modmenu/menus/quick-actions-menu.h
+++ src/modules/modmenu/menus/quick-actions-menu.h
@@ -2,17 +2,23 @@
 #include "../common.h"
 #include "../menu-base.h"
 
+static constexpr size_t ACTIONS_PER_PAGE = 8;
+
 namespace ModMenuModule {
 	class QuickActionsMenu : public MenuBase {
 	public:
-		QuickActionsMenu();
+		QuickActionsMenu(size_t page = 0);
 		virtual ~QuickActionsMenu();
 
 		virtual bool Attach() override;
 
 	private:
 		void OnMenuAction(UiModule::Selectable* item, UiModule::MenuItemId id) override;
+		std::wstring GetPageTitle(size_t pageCount) const;
 
 		UiModule::MenuItemId m_firstActionItemId = -1;
+		size_t m_page;
+		UiModule::MenuItemId m_prevPageItemId = -1;
+		UiModule::MenuItemId m_nextPageItemId = -1;
 	};
 }
