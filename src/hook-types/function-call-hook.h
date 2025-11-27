59fe119077c77c415bbeb7e1cd80b10175cd0970
izawartka <59137928+izawartka@users.noreply.github.com>
2025-11-27 21:12:30 +0100

    Fix FunctionCallHook field name
    - Changed FunctionCallHook::addresses field name to address

--- src/hook-types/function-call-hook.h
+++ src/hook-types/function-call-hook.h
@@ -3,31 +3,31 @@
 
 /*
 Replaces 5 bytes at a given address with a call instruction to a custom function.
-@param addresses The address where the call instruction will be placed.
+@param address The address where the call instruction will be placed.
 @param hookFunction The address of the custom function to call.
 */
 class FunctionCallHook : public Core::HookBase
 {
 public:
 	FunctionCallHook(DWORD address, DWORD hookFunction)
-		: addresses(address), hookFunction(hookFunction) {
+		: address(address), hookFunction(hookFunction) {
 	}
 	~FunctionCallHook() = default;
 
 	static bool Hook(const FunctionCallHook& hookDef) {
 		DWORD OldProtection = { 0 };
-		bool success = VirtualProtectEx(GetCurrentProcess(), (LPVOID)hookDef.addresses, 5, PAGE_EXECUTE_READWRITE, &OldProtection);
+		bool success = VirtualProtectEx(GetCurrentProcess(), (LPVOID)hookDef.address, 5, PAGE_EXECUTE_READWRITE, &OldProtection);
 		if (!success) {
 			return false;
 		}
 
-		*(BYTE*)((LPBYTE)hookDef.addresses) = 0xE8;
-		DWORD offset = hookDef.hookFunction - hookDef.addresses - 5;
-		*(DWORD*)((LPBYTE)hookDef.addresses + 1) = offset;
+		*(BYTE*)((LPBYTE)hookDef.address) = 0xE8;
+		DWORD offset = hookDef.hookFunction - hookDef.address - 5;
+		*(DWORD*)((LPBYTE)hookDef.address + 1) = offset;
 
 		return true;
 	}
 
-	const DWORD addresses;
+	const DWORD address;
 	const DWORD hookFunction;
 };
