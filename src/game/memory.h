271e91e0b662149b220e37fce45d4f2a80c70361
izawartka <59137928+izawartka@users.noreply.github.com>
2025-11-29 01:46:16 +0100

    Remove constexpr from memory.h
    - Game::Memory functions are no longer constexpr

--- src/game/memory.h
+++ src/game/memory.h
@@ -3,32 +3,31 @@
 #include "keycode.h"
 
 namespace Game {
-	class Memory
-	{
+	class Memory {
 	public:
-		static constexpr Game* GetGame() { return (Game*)*(DWORD*)0x005eb4fc; }
-		static constexpr Ped* GetPlayerPed() {
+		static Game* GetGame() { return (Game*)*(DWORD*)0x005eb4fc; }
+		static Ped* GetPlayerPed() {
 			Game* game = GetGame();
 			if (!game) return nullptr;
 			Player* player = game->CurrentPlayer;
 			if (!player) return nullptr;
 			return player->ped;
 		}
-		static constexpr KeyCode* GetKeyPressCode() { return (KeyCode*)0x005e8f8c; }
-		static constexpr uint* GetKeyPressState() { return (uint*)0x005e8f90; }
-		static constexpr S4* GetS4() { return (S4*)*(DWORD*)0x00671978; }
-		static constexpr Style_S3* GetStyleS3() { return (Style_S3*)*(DWORD*)0x00670684; }
-		static constexpr Cheats* GetCheats() { return (Cheats*)0x005ead51; }
-		static constexpr GangRespectContainer* GetGangRespectContainer() { return (GangRespectContainer*)*(DWORD*)0x005eb898; }
-		static constexpr short* GetDefaultFontId() { return (short*)0x00672f24; }
-		static constexpr S15_Script* GetS15() { return (S15_Script*)*(DWORD*)0x006644bc; }
-		static constexpr Menu* GetMenu() { return (Menu*)0x005ec070; }
-		static constexpr CarsManager* GetCarsManager() { return (CarsManager*)*(DWORD*)0x005e4ca0; }
-		static constexpr Camera* GetMainCamera() { return (Camera*)*(DWORD*)0x005e3cc4; }
-		static constexpr bool* GetIsFullScreen() { return (bool*)0x00595014; }
-		static constexpr int* GetFullScreenWidth() { return (int*)0x006732e4; }
-		static constexpr int* GetFullScreenHeight() { return (int*)0x006732e0; }
-		static constexpr int* GetWindowedWidth() { return (int*)0x00673578; }
-		static constexpr int* GetWindowedHeight() { return (int*)0x006732e8; }
+		static KeyCode* GetKeyPressCode() { return (KeyCode*)0x005e8f8c; }
+		static uint* GetKeyPressState() { return (uint*)0x005e8f90; }
+		static S4* GetS4() { return (S4*)*(DWORD*)0x00671978; }
+		static Style_S3* GetStyleS3() { return (Style_S3*)*(DWORD*)0x00670684; }
+		static Cheats* GetCheats() { return (Cheats*)0x005ead51; }
+		static GangRespectContainer* GetGangRespectContainer() { return (GangRespectContainer*)*(DWORD*)0x005eb898; }
+		static short* GetDefaultFontId() { return (short*)0x00672f24; }
+		static S15_Script* GetS15() { return (S15_Script*)*(DWORD*)0x006644bc; }
+		static Menu* GetMenu() { return (Menu*)0x005ec070; }
+		static CarsManager* GetCarsManager() { return (CarsManager*)*(DWORD*)0x005e4ca0; }
+		static Camera* GetMainCamera() { return (Camera*)*(DWORD*)0x005e3cc4; }
+		static bool* GetIsFullScreen() { return (bool*)0x00595014; }
+		static int* GetFullScreenWidth() { return (int*)0x006732e4; }
+		static int* GetFullScreenHeight() { return (int*)0x006732e0; }
+		static int* GetWindowedWidth() { return (int*)0x00673578; }
+		static int* GetWindowedHeight() { return (int*)0x006732e8; }
 	};
 }
