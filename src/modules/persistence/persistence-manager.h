795ee552c2867bdfff620cb6515d7c8acaf5f0de
izawartka <59137928+izawartka@users.noreply.github.com>
2025-12-25 15:10:27 +0100

    Add String and WString overloads in Persistence
    - Refactored PersistenceModule::PersistenceManager to use SaveInternal and LoadInternal methods
    - Added std::string and std::wstring overloads for Load, Save and LoadOptional methods in PersistenceModule::PersistenceManager

--- src/modules/persistence/persistence-manager.h
+++ src/modules/persistence/persistence-manager.h
@@ -10,76 +10,39 @@ namespace PersistenceModule {
 
 		// Suggested key format: "ModuleName_ClassName_VariableName"
 		template <typename T>
-		void Save(std::string key, T value) {
+		void Save(const std::string& key, const T& value) {
 			spdlog::debug("Saving value with key: {}", key);
-			std::vector<uint8_t> data(sizeof(T));
-			std::memcpy(data.data(), &value, sizeof(T));
-			m_savedValues[key] = std::make_unique<SavedValue>(key, sizeof(T), data.data());
+			SaveInternal<T>(key, value);
 		}
 
-		void SaveRaw(std::string key, const uint8_t* data, size_t size) {
+		void SaveRaw(const std::string& key, const uint8_t* data, size_t size) {
 			spdlog::debug("Saving raw data with key: {}", key);
 			m_savedValues[key] = std::make_unique<SavedValue>(key, size, data);
 		}
 
 		template <typename T>
-		T Load(std::string key, T defaultValue) {
+		T Load(const std::string& key, const T& defaultValue) {
 			spdlog::debug("Loading value with key: {}", key);
-			auto it = m_savedValues.find(key);
-			if (it == m_savedValues.end()) {
-				spdlog::debug("Key not found, returning default value");
+			auto result = LoadInternal<T>(key);
+			if (result.has_value()) {
+				return result.value();
+			} else {
+				spdlog::debug("Returning default value");
 				return defaultValue;
 			}
-
-			auto savedValue = it->second.get();
-
-			if (savedValue->data.size() != sizeof(T)) {
-				spdlog::warn("Size mismatch: {} vs {}, returning default value", savedValue->data.size(), sizeof(T));
-				return defaultValue;
-			}
-
-			T value;
-			std::memcpy(&value, savedValue->data.data(), sizeof(T));
-			return value;
 		}
 
 		template <typename T>
-		std::optional<T> LoadOptional(std::string key) {
+		std::optional<T> LoadOptional(const std::string& key) {
 			spdlog::debug("Loading value with key: {}", key);
-			auto it = m_savedValues.find(key);
-			if (it == m_savedValues.end()) {
-				spdlog::debug("Key not found, returning nullopt");
-				return std::nullopt;
-			}
-			auto savedValue = it->second.get();
-			if (savedValue->data.size() != sizeof(T)) {
-				spdlog::warn("Size mismatch: {} vs {}, returning nullopt", savedValue->data.size(), sizeof(T));
-				return std::nullopt;
-			}
-			T value;
-			std::memcpy(&value, savedValue->data.data(), sizeof(T));
-			return value;
+			return LoadInternal<T>(key);
 		}
 
-		bool LoadRaw(std::string key, uint8_t*& data, size_t& size) {
+		bool LoadRaw(const std::string& key, std::unique_ptr<uint8_t[]>& data, size_t& size) {
 			spdlog::debug("Loading raw data with key: {}", key);
 			auto it = m_savedValues.find(key);
 			if (it == m_savedValues.end()) {
-				spdlog::debug("Key not found");
-				return false;
-			}
-			auto savedValue = it->second.get();
-			size = savedValue->size;
-			data = new uint8_t[size];
-			std::memcpy(data, savedValue->data.data(), size);
-			return true;
-		}
-
-		bool LoadRaw(std::string key, std::unique_ptr<uint8_t[]>& data, size_t& size) {
-			spdlog::debug("Loading raw data with key: {}", key);
-			auto it = m_savedValues.find(key);
-			if (it == m_savedValues.end()) {
-				spdlog::debug("Key not found");
+				spdlog::warn("Key not found");
 				return false;
 			}
 			auto savedValue = it->second.get();
@@ -109,6 +72,74 @@ namespace PersistenceModule {
 			size_t size;
 			std::vector<uint8_t> data;
 		};
+
+		template <typename T>
+		void SaveInternal(const std::string& key, const T& value) {
+			std::vector<uint8_t> data(sizeof(T));
+			std::memcpy(data.data(), &value, sizeof(T));
+			m_savedValues[key] = std::make_unique<SavedValue>(key, sizeof(T), data.data());
+		}
+
+		template <>
+		void SaveInternal<std::wstring>(const std::string& key, const std::wstring& value) {
+			size_t dataSize = value.size() * sizeof(wchar_t);
+			std::vector<uint8_t> data(dataSize);
+			std::memcpy(data.data(), value.data(), dataSize);
+			m_savedValues[key] = std::make_unique<SavedValue>(key, dataSize, data.data());
+		}
+
+		template <>
+		void SaveInternal<std::string>(const std::string& key, const std::string& value) {
+			size_t dataSize = value.size() * sizeof(char);
+			std::vector<uint8_t> data(dataSize);
+			std::memcpy(data.data(), value.data(), dataSize);
+			m_savedValues[key] = std::make_unique<SavedValue>(key, dataSize, data.data());
+		}
+
+		template <typename T>
+		std::optional<T> LoadInternal(const std::string& key) {
+			auto it = m_savedValues.find(key);
+			if (it == m_savedValues.end()) {
+				spdlog::warn("Key not found");
+				return std::nullopt;
+			}
+			auto savedValue = it->second.get();
+			if (savedValue->data.size() != sizeof(T)) {
+				spdlog::error("Size mismatch: {} vs {}", savedValue->data.size(), sizeof(T));
+				return std::nullopt;
+			}
+			T value;
+			std::memcpy(&value, savedValue->data.data(), sizeof(T));
+			return value;
+		}
+
+		template <>
+		std::optional<std::wstring> LoadInternal<std::wstring>(const std::string& key) {
+			auto it = m_savedValues.find(key);
+			if (it == m_savedValues.end()) {
+				spdlog::warn("Key not found");
+				return std::nullopt;
+			}
+			auto savedValue = it->second.get();
+			size_t wcharCount = savedValue->data.size() / sizeof(wchar_t);
+			std::wstring value(wcharCount, L'\0');
+			std::memcpy(&value[0], savedValue->data.data(), savedValue->data.size());
+			return value;
+		}
+
+		template <>
+		std::optional<std::string> LoadInternal<std::string>(const std::string& key) {
+			auto it = m_savedValues.find(key);
+			if (it == m_savedValues.end()) {
+				spdlog::warn("Key not found");
+				return std::nullopt;
+			}
+			auto savedValue = it->second.get();
+			size_t charCount = savedValue->data.size() / sizeof(char);
+			std::string value(charCount, '\0');
+			std::memcpy(&value[0], savedValue->data.data(), savedValue->data.size());
+			return value;
+		}
 
 		std::unordered_map<std::string, std::unique_ptr<SavedValue>> m_savedValues;
 	};
