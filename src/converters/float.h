16420a86cba870ee428c86727ed8d1ba74b1c45f
izawartka <59137928+izawartka@users.noreply.github.com>
2026-01-07 21:29:38 +0100

    Clean up FloatConverter
    - Cleaned up FloatConverter code

--- src/converters/float.h
+++ src/converters/float.h
@@ -2,74 +2,71 @@
 #include <string>
 #include <cfenv>
 #include <type_traits>
-#include <cassert>
 #include <cmath>
 
-template <typename T, size_t precision = 3>
-class FloatConverter {
+template <typename T, size_t precision = 3,
+	bool Supported = std::is_floating_point_v<std::remove_cv_t<T>>
+>
+class FloatConverter;
+
+// Specialization for supported types
+template <typename T, size_t precision>
+class FloatConverter<T, precision, true> {
 public:
 	static std::wstring ConvertToString(T value) {
 		using BaseT = std::remove_cv_t<T>;
 		std::fesetround(FE_TONEAREST);
 
-		if constexpr (std::is_floating_point_v<BaseT>) {
-			BaseT roundFactor = static_cast<BaseT>(std::pow(10, precision));
-			BaseT roundedValue = std::round(value * roundFactor) / roundFactor;
+		BaseT roundFactor = static_cast<BaseT>(std::pow(10, precision));
+		BaseT roundedValue = std::round(value * roundFactor) / roundFactor;
 
-			std::wstring text = std::to_wstring(roundedValue);
-			size_t decimalPos = text.find(L'.');
-			if (decimalPos == std::wstring::npos) return text;
-			size_t endPos = decimalPos + 1 + precision;
-			if (endPos < text.length()) {
-				text = text.substr(0, endPos);
-			}
-			return text;
+		std::wstring text = std::to_wstring(roundedValue);
+		size_t decimalPos = text.find(L'.');
+		if (decimalPos == std::wstring::npos) return text;
+		size_t endPos = decimalPos + 1 + precision;
+		if (endPos < text.length()) {
+			text = text.substr(0, endPos);
 		}
-		else {
-#ifdef DEBUG
-			return L"<Unsupported value>";
-#else
-			return L"";
-#endif // DEBUG
-		}
+		return text;
 	}
 
 	static T ConvertFromString(std::wstring text) {
 		using BaseT = std::remove_cv_t<T>;
 		std::fesetround(FE_TONEAREST);
 
-		if constexpr (std::is_floating_point_v<BaseT>) {
-			if (text.empty()) return T{};
-			return static_cast<BaseT>(std::stod(text));
-		}
-		else {
-			assert(false && "Unsupported type for FloatConverter");
-			return T{};
-		}
+		if (text.empty()) return T{};
+		return static_cast<BaseT>(std::stod(text));
 	}
 
 	static bool IsValidChar(std::wstring text, wchar_t c) {
-		using BaseT = std::remove_cv_t<T>;
-		
-		if constexpr (std::is_floating_point_v<BaseT>) {
-			bool hasDecimal = text.find(L'.') != std::wstring::npos;
-			if (c == L'.') return !hasDecimal;
+		bool hasDecimal = text.find(L'.') != std::wstring::npos;
+		if (c == L'.') return !hasDecimal;
 
-			bool hasLength = text.length() > 0;
-			if (c == L'-') return !hasLength;
+		bool hasLength = text.length() > 0;
+		if (c == L'-') return !hasLength;
 
-			return (c >= L'0' && c <= L'9');
-		}
-		else {
-			assert(false && "Unsupported type for FloatConverter");
-			return false;
-		}
+		return (c >= L'0' && c <= L'9');
 	}
 
 	static bool AreEqual(T a, T b) {
 		T diff = std::fabs(a - b);
 		T epsilon = static_cast<T>(std::pow(10, -static_cast<int>(precision)));
 		return diff < epsilon;
+	}
+
+protected:
+	FloatConverter() = delete;
+	~FloatConverter() = delete;
+	FloatConverter(const FloatConverter&) = delete;
+	FloatConverter& operator=(const FloatConverter&) = delete;
+};
+
+// Specialization for unsupported types
+template <typename T, size_t precision>
+class FloatConverter<T, precision, false> {
+public:
+	static std::wstring ConvertToString(T value) {
+		return L"<Unsupported value>";
 	}
 
 protected:
