2448fe1d8707bbb9cd5f5e4ba367a7202dac50bc
izawartka <59137928+izawartka@users.noreply.github.com>
2026-07-04 13:23:58 +0200

    Add ScrVector3RotConverter
    - Added ScrVector3RotConverter

---
+++ src/converters/scr-vector3-rot.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "scrf.h"
+#include "gta-angle.h"
+
+class ScrVector3RotConverter {
+public:
+	static std::wstring ConvertToString(std::pair<Game::SCR_Vector3, short> value) {
+		std::wstring x = ScrfConverter::ConvertToString(value.first.x);
+		std::wstring y = ScrfConverter::ConvertToString(value.first.y);
+		std::wstring z = ScrfConverter::ConvertToString(value.first.z);
+		std::wstring rot = GtaAngleConverter<>::ConvertToString(value.second);
+
+		return L"(" + x + L", " + y + L", " + z + L", " + rot + L" deg)";
+	}
+
+	static bool AreEqual(std::pair<Game::SCR_Vector3, short> a, std::pair<Game::SCR_Vector3, short> b) {
+		return a == b;
+	}
+
+protected:
+	ScrVector3RotConverter() = delete;
+	~ScrVector3RotConverter() = delete;
+	ScrVector3RotConverter(const ScrVector3RotConverter&) = delete;
+	ScrVector3RotConverter& operator=(const ScrVector3RotConverter&) = delete;
+};
